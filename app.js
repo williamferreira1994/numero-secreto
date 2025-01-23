40b673997f8268a631991450a460821ef011b1db
wilwilpopcorn <wilwilpopcorn@outlook.com>
2025-01-23 19:54:00 -0300

    restaurando

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let listaDeNumerosSorteados = [];
+<<<<<<< HEAD
 let numeroLimite = 50;
+=======
+let numeroLimite = 10;
+>>>>>>> parent of 061bfe8 (alterando limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
