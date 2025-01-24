c3d5b6f644c94e55e53a8a04cfd2a50058a7e4af
wilwilpopcorn <wilwilpopcorn@outlook.com>
2025-01-24 19:43:19 -0300

    Adicionando o README

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let listaDeNumerosSorteados = [];
-<<<<<<< HEAD
-let numeroLimite = 50;
-=======
 let numeroLimite = 10;
->>>>>>> parent of 061bfe8 (alterando limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
